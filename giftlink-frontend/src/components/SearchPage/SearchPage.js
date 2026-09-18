512294fe90d32e47b9d0d0546e00f669625e4254
Waleed <vvaleed@gmail.com>
2026-09-18 17:07:13 +0500

    feat: implement SearchPage component for searching and filtering gifts

--- giftlink-frontend/src/components/SearchPage/SearchPage.js
+++ giftlink-frontend/src/components/SearchPage/SearchPage.js
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {urlConfig} from '../../config';
+import { urlConfig } from '../../config';
 
 function SearchPage() {
-
-    //Task 1: Define state variables for the search query, age range, and search results.
-    const categories = ['Living', 'Bedroom', 'Bathroom', 'Kitchen', 'Office'];
-    const conditions = ['New', 'Like New', 'Older'];
-	const [searchQuery, setSearchQuery] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
     const [ageRange, setAgeRange] = useState(6); // Initialize with minimum value
     const [searchResults, setSearchResults] = useState([]);
+    const categories = ['Living', 'Bedroom', 'Bathroom', 'Kitchen', 'Office'];
+    const conditions = ['New', 'Like New', 'Older'];
 
     useEffect(() => {
         // fetch all products
@@ -34,8 +32,7 @@ function SearchPage() {
     }, []);
 
 
-    // Task 2. Fetch search results from the API based on user inputs.
-	const handleSearch = async () => {
+    const handleSearch = async () => {
         // Construct the search URL based on user input
         const baseUrl = `${urlConfig.backendUrl}/api/search?`;
         const queryParams = new URLSearchParams({
@@ -61,11 +58,7 @@ function SearchPage() {
 
     const goToDetailsPage = (productId) => {
         navigate(`/app/product/${productId}`);
-
     };
-
-
-
 
     return (
         <div className="container mt-5">
@@ -74,67 +67,69 @@ function SearchPage() {
                     <div className="filter-section mb-3 p-3 border rounded">
                         <h5>Filters</h5>
                         <div className="d-flex flex-column">
-                            	{/* Category Dropdown */}
-	<label htmlFor="categorySelect">Category</label>
-	<select id="categorySelect" className="form-control my-1">
-		<option value="">All</option>
-		{categories.map(category => (
-		<option key={category} value={category}>{category}</option>
-		))}
-	</select>
+                            {/* Category Dropdown */}
+                            <label htmlFor="categorySelect">Category</label>
+                            <select id="categorySelect" className="form-control my-1">
+                                <option value="">All</option>
+                                {categories.map(category => (
+                                    <option key={category} value={category}>{category}</option>
+                                ))}
+                            </select>
 
-	{/* Condition Dropdown */}
-	<label htmlFor="conditionSelect">Condition</label>
-	<select id="conditionSelect" className="form-control my-1">
-		<option value="">All</option>
-		{conditions.map(condition => (
-		<option key={condition} value={condition}>{condition}</option>
-		))}
-	</select>
+                            {/* Condition Dropdown */}
+                            <label htmlFor="conditionSelect">Condition</label>
+                            <select id="conditionSelect" className="form-control my-1">
+                                <option value="">All</option>
+                                {conditions.map(condition => (
+                                    <option key={condition} value={condition}>{condition}</option>
+                                ))}
+                            </select>
 
-                           	{/* Age Range Slider */}
-	<label htmlFor="ageRange">Less than {ageRange} years</label>
-	<input
-		   type="range"
-		   className="form-control-range"
-		   id="ageRange"
-		   min="1"
-		   max="10"
-		   value={ageRange}
-		   onChange={e => setAgeRange(e.target.value)}
-	/>
- <input type="text"   className="form-control mb-2"
-                        placeholder="Search for items..."
-                        value={searchQuery}
-                    onChange={e=>setSearchQuery(e.target.value)}/>
+                            {/* Age Range Slider */}
+                            <label htmlFor="ageRange">Less than {ageRange} years</label>
+                            <input
+                                type="range"
+                                className="form-control-range"
+                                id="ageRange"
+                                min="1"
+                                max="10"
+                                value={ageRange}
+                                onChange={e => setAgeRange(e.target.value)}
+                            />
                         </div>
                     </div>
-                   
-                    {<button id="search" onClick={handleSearch()}/>}
-                    	<div className="search-results mt-4">
-		{searchResults.length > 0 ? (
-		searchResults.map(product => (
-		<div key={product.id} className="card mb-3">
-			{/* Check if product has an image and display it */}
-			<img src={product.image} alt={product.name} className="card-img-top" />
-			<div className="card-body">
-				<h5 className="card-title">{product.name}</h5>
-				<p className="card-text">{product.description.slice(0, 100)}...</p>
-			</div>
-			<div className="card-footer">
-				<button onClick={() => goToDetailsPage(product.id)} className="btn btn-primary">
-					View More
-				</button>
-			</div>
-		</div>
-		))
-		) : (
-		<div className="alert alert-info" role="alert">
-			No products found. Please revise your filters.
-		</div>
-		)}
-	</div>
 
+                    <input
+                        type="text"
+                        className="form-control mb-2"
+                        placeholder="Search for items..."
+                        value={searchQuery}
+                        onChange={e => setSearchQuery(e.target.value)}
+                    />
+                    <button className="btn btn-primary" onClick={handleSearch}>Search</button>
+                    <div className="search-results mt-4">
+                        {searchResults.length > 0 ? (
+                            searchResults.map(product => (
+                                <div key={product.id} className="card mb-3">
+                                    {/* Check if product has an image and display it */}
+                                    <img src={product.image} alt={product.name} className="card-img-top" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{product.name}</h5>
+                                        <p className="card-text">{product.description.slice(0, 100)}...</p>
+                                    </div>
+                                    <div className="card-footer">
+                                        <button onClick={() => goToDetailsPage(product.id)} className="btn btn-primary">
+                                            View More
+                                        </button>
+                                    </div>
+                                </div>
+                            ))
+                        ) : (
+                            <div className="alert alert-info" role="alert">
+                                No products found. Please revise your filters.
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
