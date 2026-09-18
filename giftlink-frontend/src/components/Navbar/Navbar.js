67625b5941a2d7dcd1399cc2e3b890275c2c74aa
Waleed <vvaleed@gmail.com>
2026-09-18 17:17:59 +0500

    feat: add Navbar component with navigation links

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
                         <a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}
                     </li>
                     <li className="nav-item">
-	            <a className="nav-link" to="/app/search">Search</a>
+	            <a className="nav-link" href="/app/search">Search</a>
 </li>
 
 
